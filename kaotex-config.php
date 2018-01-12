ff90ea93fed6c6cf0293b4afb18a61e48317f629
Sam Morrell <samuelmorrell@gmail.com>
2018-01-12 10:42:58 +0000

    Initial commit of plugin code.

---
+++ kaotex-config.php
@@ -0,0 +1,9 @@
+<?php
+
+$config = array(
+	"katex-cdn-url" => "https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.8.3/katex.min.js",
+	"katex-auto-render-cdn-url" => "https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.8.3/contrib/auto-render.min.js",
+	"katex-css-crn-url" => "https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.8.3/katex.min.css"
+);
+
+?>
