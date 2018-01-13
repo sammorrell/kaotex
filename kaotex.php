3cfcea66b643b978d14ee98ed2ffdc34bb65c705
Sam Morrell <smorrell@Herschel.localdomain>
2018-01-13 21:11:25 +0000

    Did some work on metadata and basic plugin functions.

--- kaotex.php
+++ kaotex.php
@@ -1,3 +1,31 @@
+<?php
+/*
+		Plugin Name: KaoTeX
+		Plugin URI: 
+		Description: A flexible, easy-to-setup WordPress plugin to use KaTeX to render LaTeX maths expressions on your website. 
+		Author: Sam Morrell
+		Author URI: https://www.sammorrell.co.uk/
+		Copyright: Sam Morrell
+		License: MIT
+		Version: 1.0.0
+*/
+
 <?php defined( 'ABSPATH' ) or die( 'No script kiddies please!' ); ?>
 
+<?php
 include("kaotex-config.php");
+
+
+add_action('init', 'kaotex_init');
+add_action('wp_enqueue_scripts', 'kaotex_head_scripts');
+
+
+function kaotex_init() {
+	
+}
+
+function kaotex_head_scripts() {
+	
+}
+
+?>
---
+++ readme.txt
@@ -0,0 +1,5 @@
+=== KaoTeX ===
+Contributors: sammorrell
+Tags: latex, maths, equations, katex
+Version: 1.0.0
+License: MIT
