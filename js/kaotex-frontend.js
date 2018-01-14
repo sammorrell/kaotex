a327cbd04c306d39bac14f68205526457fbcf0d5
Sam Morrell <smorrell@Herschel.localdomain>
2018-01-14 03:21:02 +0000

    Added some tweaks to hard coded settings to try and get it working better.

--- js/kaotex-frontend.js
+++ js/kaotex-frontend.js
@@ -1,3 +1,11 @@
+
 document.addEventListener("DOMContentLoaded", function() {
-  renderMathInElement(document.body);
+	console.log("Rendering Maths...");
+	renderMathInElement(document.getElementById('main-content'), {
+		"delimiters" : [
+			{left: "$$", right: "$$", display: true},
+			{left: "$", right: "$", display: false},
+			{left: "\\begin\{equation\}", right: "\\end\{equation\}", display: true}
+		]
+	});
 });
